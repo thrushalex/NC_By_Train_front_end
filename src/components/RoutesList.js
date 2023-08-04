3887011b2f8e11d7da16a3f179a757d101d5cfd6
thrushalex <thrushalex@gmail.com>
2023-08-04 12:20:48 -0400

    Install redux and create required files

--- src/components/RoutesList.js
+++ src/components/RoutesList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import RoutesDataService from "../services/routes";
 
 const RoutesList = props => {
+
     return (
         <div className="App">
             Placeholder text for RoutesList
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
