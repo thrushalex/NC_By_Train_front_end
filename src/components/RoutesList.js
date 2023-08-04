07930ae6fd2e31346b496517a1366635e4ca9bf9
thrushalex <thrushalex@gmail.com>
2023-08-04 15:19:07 -0400

    Add functionality to retrieve and display route

--- src/components/RoutesList.js
+++ src/components/RoutesList.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import RoutesDataService from "../services/routes";
 
 const RoutesList = props => {
 
+    const [routes, setRoutes] = useState([]);
+
+    const retrieveRoutes = useCallback(() => {
+        RoutesDataService.getAll()
+        .then(response => {
+            setRoutes(response.data.routesList)
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }, [])
+
+    useEffect(() => {
+        retrieveRoutes();
+    }, [])
+
     return (
         <div className="App">
             Placeholder text for RoutesList
+            { routes.map((route, index) => {
+                return (
+                    <h5>
+                        {route.name}
+                        {route.description}
+                        {route.cities}
+                    </h5>
+                )
+            })}
         </div>
     )
 }
