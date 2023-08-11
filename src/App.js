8a95062cd9a7ef81376b7dd20ccb1308cf9f4675
thrushalex <thrushalex@gmail.com>
2023-08-11 16:09:25 -0400

    Add persistant login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import { Navbar, Nav } from "react-bootstrap";
@@ -18,6 +18,21 @@ const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 function App() {
 
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    let loginData = JSON.parse(localStorage.getItem("login"));
+    if (loginData) {
+      let loginExp = loginData.exp;
+      let now = Date.now()/1000;
+      if (now < loginExp) {
+        // Not expired
+        setUser(loginData);
+      } else {
+        // Expired
+        localStorage.setItem("login", null);
+      }
+    }
+  }, []);
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
