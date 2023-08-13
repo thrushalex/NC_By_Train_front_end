7ef44bf880425464f8b736ddfbf800f0d4fc80d4
thrushalex <thrushalex@gmail.com>
2023-08-13 17:59:30 -0400

    Add profile files

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Button from 'react-bootstrap/Button';
+
+import './Profile.css';
+
+const Profile = props => {
+
+
+    
+}
+
+export default Profile;
